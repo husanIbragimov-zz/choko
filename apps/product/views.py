899eb6cea1cc85e575dfd4fbec2be00b9be63cc3
Husan Ibragimov <ibragmovhusan283@gmail.com>
2023-03-06 18:45:53 +0500

    shop_list and shop_grid_left

--- apps/product/views.py
+++ apps/product/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.views import View
 from django.shortcuts import render
 from apps.product.models import Category, Banner, Brand, Tag, Product, Rate, Advertisement
+from django.core.paginator import Paginator
 
 
 class IndexView(View):
@@ -28,7 +29,7 @@ class IndexView(View):
 
         context = {
             'advertisements': advertisements[:1],
-            'discounts': query[:1],
+            'discounts': query[2:3],
             'queryset': query[:2],
 
             'products': product[:12],
@@ -48,4 +49,35 @@ def about(request):
 
 
 def shop_list(request):
-    return render(request, 'shop-grid-left.html', {})
+    products = Product.objects.all().order_by('-id')
+    category = Category.objects.filter(is_active=True)
+    brands = Brand.objects.all().order_by('-id')
+    top_rate_products = products.order_by('-mid_rate')
+    last_3_products = products.order_by('-view')
+
+    # filter
+    cat = request.GET.get('cat')
+    top_rated = request.GET.get('top_rated')
+    if cat:
+        products = products.filter(category__title__icontains=cat)
+
+    query = []
+    for qs in products:
+        if qs.percentage > 20:
+            query.append(qs)
+
+    # paginator
+    paginator = Paginator(products, 15)
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
+
+    context = {
+        'products': products,
+        'discounts': query,
+        'page_obj': products,
+        'categories': category,
+        'brands': brands,
+        'last_3_products': last_3_products[:3],
+        'top_rate_products': top_rate_products
+    }
+    return render(request, 'shop-grid-left.html', context)
