1e652957b197b3be467ae8e1ea0dd87ca273b131
Diyorbek <diyorbeky610@gmail.com>
2023-03-23 18:02:43 +0500

    fix some bugs

--- apps/product/views.py
+++ apps/product/views.py
@@ -124,12 +124,12 @@ def shop_details(request, id):
     related_products = Product.objects.filter(~Q(id=product.id), category__in=[i.id for i in product.category.all()],
                                               is_active=True)
     images = ProductImage.objects.raw(
-        '''SELECT id,
-            count(color_id)
+        '''SELECT id, color_id,
+            count(color_id) as number_colors
             FROM product_productimage
             WHERE product_id = %s 
             GROUP by color_id
-            ORDER By count(color_id) desc''',
+            ORDER By number_colors desc''',
         [id])
     images_ = ProductImage.objects.filter(product_id=12).values('color').annotate(count=Count('color'))
     for image in images_:
