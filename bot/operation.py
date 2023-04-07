a3ff88fc031893659ebf7f149f2f6c69d55261be
Diyorbek <diyorbeky610@gmail.com>
2023-04-08 00:50:03 +0500

    changes

--- bot/operation.py
+++ bot/operation.py
@@ -23,7 +23,8 @@ chat_id = '-1001906730536'
 
 @dp.message_handler(Command('count'))
 async def bot_help(message: types.Message):
-    response = requests.request("GET", URL_PRODUCT)
+    response = requests.request("GET", URL_SERVER_PRODUCT)
+    print(response)
     if response.status_code == 200:
         await bot.send_message('-1001906730536', f"Mahsulotlar soni: <b>{response.json().get('msg')}</b> ta", parse_mode='html')
     else:
