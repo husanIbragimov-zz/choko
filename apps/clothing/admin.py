c8d366a38f09b8af25fe0bf77ffc464444c69172
husanibragimov <ibragimovdev@tstu.uz>
2023-09-27 18:18:39 +0500

    clothing admin

--- apps/clothing/admin.py
+++ apps/clothing/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Tag, Clothing
 
-# Register your models here.
+
+class ClothingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'status', 'is_active')
+    list_display_links = ('id', 'title')
+    filter_horizontal = ('tags', 'size')
+    list_editable = ('is_active',)
+    search_fields = ('title', 'description')
+    list_per_page = 25
+
+
+admin.site.register(Tag)
+admin.site.register(Clothing, ClothingAdmin)
