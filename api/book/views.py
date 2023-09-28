f1695c85d82d0ab1820e1a0e7733133e697c81d1
husanibragimov <ibragimovdev@tstu.uz>
2023-09-28 19:00:24 +0500

    rm tag serializer

--- api/book/views.py
+++ api/book/views.py
@@ -1,17 +1,17 @@
 from apps.product.models import *
-from .serializers import AuthorSerializer, BookCreateSerializer, BookImageSerializer, BookListSerializer, BookSerializer, PrintedSerializer
+from .serializers import AuthorSerializer, BookCreateSerializer, BookImageSerializer, BookListSerializer, \
+    BookSerializer, PrintedSerializer
 from rest_framework import mixins, generics, status, viewsets, response
 
+
 class BookModelViewSet(mixins.CreateModelMixin,
-                     mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.ListModelMixin,
-                     viewsets.GenericViewSet):
+                       mixins.RetrieveModelMixin,
+                       mixins.UpdateModelMixin,
+                       mixins.ListModelMixin,
+                       viewsets.GenericViewSet):
     queryset = Product.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'id'
-
-
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -19,33 +19,35 @@ class BookModelViewSet(mixins.CreateModelMixin,
         elif self.action == 'retrieve':
             return BookSerializer
         elif self.action == 'create':
-           return BookCreateSerializer
+            return BookCreateSerializer
         return BookSerializer
-    
+
     def create(self, request, *args, **kwargs):
         data = request.data
         sz_ = BookCreateSerializer(data=data)
         sz_.is_valid(raise_exception=True)
         sz_.save()
         images = {
-           
+
         }
         files = request.FILES
         for file in files:
             images[file] = []
             for i in files.getlist(file):
-
-                sz = BookImageSerializer(data={'image': i,'wrapper':str(file), 'product': sz_.data['id'], 'price':data[f'price_{str(file)}']})
+                sz = BookImageSerializer(data={'image': i, 'wrapper': str(file), 'product': sz_.data['id'],
+                                               'price': data[f'price_{str(file)}']})
                 sz.is_valid(raise_exception=True)
                 sz.save()
                 images[file].append(sz.data)
 
-        return response.Response({'data':sz_.data,'images':images}, status=status.HTTP_201_CREATED)
-    
+        return response.Response({'data': sz_.data, 'images': images}, status=status.HTTP_201_CREATED)
+
+
 class ProductImageViewSet(generics.RetrieveUpdateDestroyAPIView):
     queryset = ProductImage.objects.all()
     serializer_class = BookImageSerializer
     lookup_field = 'id'
+
 
 class AuthorModelViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
@@ -57,5 +59,3 @@ class PrintedModelViewSet(viewsets.ModelViewSet):
     queryset = Printed.objects.all()
     serializer_class = PrintedSerializer
     lookup_field = 'id'
-
-
