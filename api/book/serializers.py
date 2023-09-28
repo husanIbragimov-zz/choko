1f2f43d07efe59436bebd3f0001cbecf8876ee96
OQdevpy <o.qosimov@tuit.uz>
2023-09-28 19:11:58 +0500

    odev

--- api/book/serializers.py
+++ api/book/serializers.py
@@ -35,18 +35,26 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField(read_only=True)
     tags  = TagSerializer(many=True, read_only=True)
     author = serializers.CharField(source='author.title', read_only=True)
     advertisement = serializers.CharField(source='advertisement.title', read_only=True)
     banner_discount = serializers.CharField(source='banner_discount.title', read_only=True)
     category = CategorySerializer(many=True, read_only=True)
-    product_images = serializers.ManyRelatedField(child_relation=BookImageSerializer(), read_only=True)
 
+
+    def get_images(self, obj):
+        yumshoq = obj.product_images.filter(wrapper='yumshoq')
+        qattiq = obj.product_images.filter(wrapper='qattiq')
+        return {
+            'yumshoq': BookImageSerializer(yumshoq, many=True).data,
+            'qattiq': BookImageSerializer(qattiq, many=True).data
+        }
 
     class Meta:
         model = Product
-        fields = ['id', 'title','product_images', 'banner_discount', 'advertisement', 'status', 'category',
-                  'author', 'description',  'availability', 'brand',
+        fields = ['id', 'title','images', 'banner_discount', 'advertisement', 'status', 'category',
+                  'author', 'description', 'availability', 'brand',
                   'percentage', 'discount', 'view', 'isbn', 'author', 'lang', 'script', 'total_pages',
                   'printed', 'format', 'year_of_creation', 'tags', 'product_type', 'created_at', 'updated_at', 'is_active']
 
