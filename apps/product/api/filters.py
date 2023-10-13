4d28cb0e23032002bc9c11ce651faa2a6bea16ad
husan <ibragimovdev@tstu.uz>
2023-10-13 19:23:11 +0500

    filter products

---
+++ apps/product/api/filters.py
@@ -0,0 +1,24 @@
+import datetime
+import django_filters
+from django_filters import rest_framework as filters
+from django.db.models import Q
+from apps.product.models import Brand, Category, Product, PRODUCT_TYPE, Size, Author, Color, ProductImage
+
+
+class AppProductFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+    brand = django_filters.ModelMultipleChoiceFilter(queryset=Brand.objects.all())
+    category = django_filters.ModelChoiceFilter(queryset=Category.objects.filter(parent__isnull=False))
+    product_type = django_filters.ChoiceFilter(choices=PRODUCT_TYPE, field_name='product_type', lookup_expr='exact')
+    author = django_filters.ModelChoiceFilter(queryset=Author.objects.all(), field_name='author', lookup_expr='exact')
+    size = django_filters.ModelMultipleChoiceFilter(queryset=Size.objects.all())
+    color = filters.ModelMultipleChoiceFilter(queryset=Color.objects.all(), method='filter_color')
+
+    def filter_color(self, queryset, name, value):
+        colors = ProductImage.objects.filter(color_id__in=value)
+        qs = queryset.filter(product_images__in=colors)
+        return qs
+
+    class Meta:
+        model = Product
+        fields = ['title', 'category', 'brand', 'size', 'color', 'author', 'product_type']
